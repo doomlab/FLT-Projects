cf3e004808fb693b67e2e7a3a56b557359a7447e
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2020-06-22 14:37:11 -0400

    add subject files

---
+++ input_data/mturk/reader_csv.pl
@@ -0,0 +1,24 @@
+$fileopen = "transpose04.txt";
+
+open(FIN,$fileopen) or (print "File $fileopen not found\n");
+while (<FIN>){
+
+	chomp;
+	tr/ \t/ /s;
+	tr/A-Z/a-z/;
+	tr/,/ /s;
+
+($target, @line) = split;
+
+print $target, " \n";
+
+open (FOUT, ">>$target.txt");
+
+for ($i=0;$i<@line;$i++)
+{
+	print FOUT $line[$i], " ";
+}
+
+print FOUT "\n";
+close (FOUT);
+	}
